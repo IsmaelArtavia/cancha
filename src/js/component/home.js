a8c31978586445e60a201259bdf41c9fa80dc64f
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-04-24 06:55:08 +0000

    act

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,6 +10,22 @@ export function Home() {
 		<div className="text-center mt-5">
 			<div className="cancha">
 				<div className="bola"></div>
+
+				<div className="circuloCentral"></div>
+				<div className="rayaCentral"></div>
+				<div className="areaGrandeIzquierda"></div>
+				<div className="areaPequenaIzquierda"></div>
+				<div className="semiCirculoIzquierda"></div>
+				<div className="areaGrandeDerecha"></div>
+				<div className="areaPequenaDerecha"></div>
+				<div className="semiCirculoDerecha"></div>
+				<div className="circuloPenalIzquierda"></div>
+				<div className="circuloPenalDerecha"></div>
+				<div className="cornerArribaIzquierda"></div>
+				<div className="cornerAbajoIzquierda"></div>
+				<div className="cornerArribaDerecha"></div>
+				<div className="cornerAbajoDerecha"></div>
+
 				<canvas width="400" height="400" id="lienzo"></canvas>
 			</div>
 		</div>
